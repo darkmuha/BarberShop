e8f57af719af9f41185b71ad68c24d2c62f46d78
darkmuha <darkmuha10@gmail.com>
2022-05-02 20:57:57 +0200

    added helmet and xss for security

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const dotenv = require('dotenv').config()
 const colors = require('colors')
 const morgan = require('morgan')
 const errorHandler = require('./middleware/error')
-const connectDB = require('./config/db')
-const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cookieParser = require('cookie-parser')
+const connectDB = require('./config/db')
 
 //Connect to database
 connectDB()
@@ -31,6 +33,12 @@ if (process.env.NODE_ENV === 'development') {
 // Sanitize data
 // it helps against nosql injection
 app.use(mongoSanitize())
+
+// Set security headers
+app.use(helmet())
+
+// Prevents XSS attacks
+app.use(xss())
 
 // Mount routers
 app.use('/api/v1/appointments', appointments)
